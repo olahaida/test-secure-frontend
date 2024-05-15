1805f10805ee1c77966129d6249d89980b134ccf
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-05-15 10:20:06 +0200

    ti10

--- cypress/e2e/api/login.api.cy.js
+++ cypress/e2e/api/login.api.cy.js
@@ -12,6 +12,37 @@ describe('Login API tests', () => {
             },
         }).then((response) => {
             expect(response.status).to.equal(200)
+            expect(response.body.token).not.to.be.undefined
+        })
+    })
+
+    it('should return HTTP 422 on invalid login', () => {
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:4001/users/signin',
+            failOnStatusCode: false,
+            body: {
+                username: 'wrong',
+                password: 'wrong'
+            }
+        }).then((response) => {
+            expect(response.status).to.eql(422)
+            expect(response.body.message).to.eql('Invalid username/password supplied')
+        })
+    })
+
+    it('should return HTTP 400 on username too short', () => {
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:4001/users/signin',
+            failOnStatusCode: false,
+            body: {
+                username: '123',
+                password: 'wrong'
+            }
+        }).then((response) => {
+            expect(response.status).to.eql(400)
+            expect(response.body.username).to.eql('Minimum username length: 4 characters')
         })
     })
 })
