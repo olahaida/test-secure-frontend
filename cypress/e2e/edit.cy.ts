11c8c9ac5e77f76f7731f70b6cf7863bf04b06c5
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-05-21 12:49:36 +0200

    rxj07

--- cypress/e2e/edit.cy.ts
+++ cypress/e2e/edit.cy.ts
@@ -20,11 +20,45 @@ describe('Edit page tests', () => {
     })
 
     it('should correctly autofill user data', () => {
+        // then
         cy.get("input[name='firstName']").should('have.value', user.firstName);
         cy.get("input[name='lastName']").should('have.value', user.lastName);
         cy.get("input[name='email']").should('have.value', user.email);
         cy.get("input[name='username']").should('have.value', user.username);
         cy.get("input[name='roles']").should('have.value', user.roles.join(','));
+    })
+
+    it.only('should successfully edit an user', () => {
+        // given
+        const newUser = getRandomUser()
+
+        // when
+        cy.get("input[name='firstName']").clear().type(newUser.firstName)
+        cy.get("input[name='lastName']").clear().type(newUser.lastName)
+        cy.get("input[name='email']").clear().type(newUser.email)
+        cy.get('.btn-primary').click()
+
+        // then
+        cy.get('.alert-success').should('have.text', 'Updating user successful')
+        cy.get('li').contains(`${newUser.firstName} ${newUser.lastName}`).should('exist')
+        cy.get('li').contains(`${user.firstName} ${user.lastName}`).should('not.exist')
+
+        cy.get('@jwtToken').then((token) => {
+            cy.request({
+                method: 'GET',
+                url: `http://localhost:4001/users/${user.username}`,
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }).then((response) => {
+                expect(response.body.username).to.eq(user.username)
+                expect(response.body.roles).to.deep.eq(user.roles)
+                expect(response.body.firstName).to.eq(newUser.firstName)
+                expect(response.body.lastName).to.eq(newUser.lastName)
+                expect(response.body.email).to.eq(newUser.email)
+            })  
+        })
+              
     })
 
 })
